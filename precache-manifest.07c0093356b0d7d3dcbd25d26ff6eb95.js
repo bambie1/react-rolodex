4fcdd0fa084b442c69662cae918b573d6433ddbd
Benaiah Ambie-Barango <bennieb96@gmail.com>
2020-04-22 13:01:01 -0400

    Updates

---
+++ precache-manifest.07c0093356b0d7d3dcbd25d26ff6eb95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f578ea15519285e3e24058531476cdec",
+    "url": "/react-rolodex/index.html"
+  },
+  {
+    "revision": "8ed2ddef7f4dd27969f5",
+    "url": "/react-rolodex/static/css/main.afd61805.chunk.css"
+  },
+  {
+    "revision": "bd4da6ab1d9e405c24fb",
+    "url": "/react-rolodex/static/js/2.e552c0ac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-rolodex/static/js/2.e552c0ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ed2ddef7f4dd27969f5",
+    "url": "/react-rolodex/static/js/main.ee3f0aa6.chunk.js"
+  },
+  {
+    "revision": "6eeb3c6d3bf16fdae6e0",
+    "url": "/react-rolodex/static/js/runtime-main.5104e1ff.js"
+  }
+]);
